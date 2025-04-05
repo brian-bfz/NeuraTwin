a4d8fbf0e0fd01d5a56a9fc8fd945b609e5f0c5f
Brian Zhao <brianvanzhao@gmail.com>
2025-04-05 00:04:56 -0400

    2D -> 3D

--- generate_data.py
+++ generate_data.py
@@ -58,8 +58,8 @@ if __name__ == "__main__":
         type=str,
         default="./gaussian_output",
     )
-    parser.add_argument("--case_name", type=str, default="double_lift_cloth_3")
-    parser.add_argument("--n_ctrl_parts", type=int, default=2)
+    parser.add_argument("--case_name", type=str)
+    parser.add_argument("--n_ctrl_parts", type=int)
     args = parser.parse_args()
 
     base_path = args.base_path
